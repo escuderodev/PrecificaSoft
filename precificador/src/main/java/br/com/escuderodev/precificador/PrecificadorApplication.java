e1b8c4d8a80058c96bf9ff414a14d63b704fab15
escuderodev <escudero.ti@gmail.com>
2024-01-02 16:56:28 -0300

    feat: criação da tela cadastro de insumos

---
+++ precificador/src/main/java/br/com/escuderodev/precificador/PrecificadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.escuderodev.precificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecificadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecificadorApplication.class, args);
+	}
+
+}
---
+++ precificador/src/test/java/br/com/escuderodev/precificador/PrecificadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escuderodev.precificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecificadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
